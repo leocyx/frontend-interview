421ed40117e9a3f7e8f790bc28105ba882260c03
leocyx <leo8400725@gmail.com>
2026-04-29 18:22:46 +0800

    feat: 可支援 4-8 位數字驗證碼（預設 6 位）

--- components/base/InputOtp.vue
+++ components/base/InputOtp.vue
@@ -1,5 +1,44 @@
-<script lang="ts">
+<script setup lang="ts">
+interface Props {
+  length: number,
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  length: () => 6
+})
+const {length} = toRefs(props);
+
+const safeLength = computed(() => Math.min(Math.max(length.value, 4), 8)) ;
+
+const otpNumber = ref<(number | undefined)[]>([]);
+
+const inputRefs = useTemplateRef<HTMLInputElement[]>('input-refs')
+
+const onNumberInput = ()=>{
+//  do someting
+};
 </script>
 <template>
-  <div>OtpComponent</div>
+  <div
+    class="min-h-screen bg-gray-100 flex items-center justify-center px-4">
+    <div class="flex flex-col items-center gap-4 w-full max-w-xs">
+      <div class="flex gap-2 w-full">
+        <div
+          v-for="(item, idx) in safeLength"
+          class="flex-1 aspect-square"
+        >
+          <input
+            :value="otpNumber[idx]"
+            ref="input-refs"
+            class="w-full h-full border-2 rounded-md text-center bg-white"
+            name="otpNumber"
+            type="text"
+            @input="onNumberInput"
+          >
+        </div>
+      </div>
+      <span class="text-red-500 text-sm">Invalid code</span>
+      <button class="w-full border-2 rounded-md bg-white py-2 ">Submit</button>
+    </div>
+  </div>
 </template>
